c6ae5a29da2f102b31f0d193da8ba91190c750eb
acabrun <acabrun@gmail.com>
2019-06-06 18:48:18 +0200

    update with click event

--- js/anniversaire.js
+++ js/anniversaire.js
@@ -1,16 +1,30 @@
-let indice = 0;
-$("p:gt(0)").hide();
-
+/*chargement du son*/
 let audio = new Audio('http://soundbible.com/grab.php?id=1817&type=mp3');
 
-$('button').on('click', function () {
-	indice++;
-	if (indice <= 3) {
-		$(`p:eq(${indice})`).show();
-	} else {
-		$('p').text('Happy Birthday');
-		$('body').css('background-image', 'url("https://tikkido.com/sites/all/modules/twtevents/images/userfiles/images/lisa-frank23.jpg")')
+/*on cache les 3 derniers paragraphes*/
+$("p:gt(0)").hide();
 
-		audio.play();
-	}
+/*Ajout des différents évènements*/
+$('button').on('click', function () {
+	$('p:eq(1)').show();
+	$('button').remove();
 });
+
+$('p:eq(1)').on('click', function () {
+	$('p:eq(2)').show();
+});
+
+$('p:eq(2)').on('click', function () {
+	$('p:eq(3)').show();
+});
+
+$('p:eq(3)').on('click', function () {
+	$('p').text('Happy Birthday');
+	$('p').css('background-color', 'pink');
+	$('p').css('color', 'white');
+	$('p').css('text-shadow', '1px 1px 1px black');
+	$('body').css('background-image', 'url("https://tikkido.com/sites/all/modules/twtevents/images/userfiles/images/lisa-frank23.jpg")')
+
+	audio.play();
+});
+
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Réalisation d'une carte d'anniversaire interactive au moyen de JQuery.
+
+Dès le début on charge le son de fin, pour éviter un temps de latence lorsqu'on clique sur le dernier paragraphe.
+
+Ajout des différents évènements.
+
+Ajout d'une image en background et du son lorsq'on clique sur le dernier paragraphe.
